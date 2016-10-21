caec70c1efcc71060f13bfbdece280927c499c29
Soujirou152 <her.pengxue@gmail.com>
2016-10-21 15:55:06 -0500

    Finished #2 and attempted #3

--- 602_hw_3/src/Dedupe.java
+++ 602_hw_3/src/Dedupe.java
@@ -5,20 +5,48 @@ public class Dedupe
 
 	public static void main(String[] args)
 	{
-		Integer i1,i2,i3,i4,i5;
-		// StdOut.println ("Enter integers");
-		
-		i1 = StdIn.readInt();
-		i2 = StdIn.readInt();
-		StdOut.println (i1 + " " + i2);
-		
 		ArrayList<Integer> list1 = new ArrayList<Integer> (); // create a new empty ArrayList
 
 		ArrayList<Integer> list2 = new ArrayList<Integer> (); // create a new empty ArrayList
 		
+		Integer i1,i2,i3,i4,i5;
+		 StdOut.print ("Enter five integers for list1: ");
+		
+		i1 = StdIn.readInt();
+		list1.add(i1);
+		i2 = StdIn.readInt();
+		list1.add(i2);
+		i3 = StdIn.readInt();
+		list1.add(i3);
+		i4 = StdIn.readInt();
+		list1.add(i4);
+		i5 = StdIn.readInt();
+		list1.add(i5);
+		//StdOut.println (i1 + " " + i2);
+		
+		 StdOut.print ("Enter five integers for list2: ");
+
+		 
+			i1 = StdIn.readInt();
+			list2.add(i1);
+			i2 = StdIn.readInt();
+			list2.add(i2);
+			i3 = StdIn.readInt();
+			list2.add(i3);
+			i4 = StdIn.readInt();
+			list2.add(i4);
+			i5 = StdIn.readInt();
+			list2.add(i5);
+				
+		
 		ArrayList<Integer> res;
 		
 		res = union(list1,list2);
+		
+		StdOut.print("The combined list is ");
+		for(Integer iterator : res){
+			StdOut.print(iterator + " ");
+		}
 		
 		// print out res as requested
 
@@ -34,10 +62,11 @@ public class Dedupe
 			result.add(elt);
 		}
 		
-//		for (int i=0; i < list1.size(); i++)
-//		{
-//			list1.get(i); // refers to same element as in elt above..
-//		}
+		for (int i=0; i < list2.size(); i++)
+		{
+			int x = list2.get(i); // refers to same element as in elt above..
+			result.add(x);
+		}
 		
 		return result;
 		
--- 602_hw_3/src/TurtleDrawing.java
+++ 602_hw_3/src/TurtleDrawing.java
@@ -1,6 +1,15 @@
 
-public class TurtleDrawing
+public abstract class TurtleDrawing
 {
+	protected Turtle turtle;
+	
+	TurtleDrawing(Turtle turt){
+		turtle = turt;
+	}
+	
+	public abstract void draw();
+	
+	
 	
 	public static void main(String[] args)
 	{
@@ -8,4 +17,34 @@ public class TurtleDrawing
 
 	}
 
+}
+
+class t1 extends TurtleDrawing{
+	
+	t1(Turtle turt) {
+		super(turt);
+		super.turtle = turt;
+
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
+class t2 extends TurtleDrawing{
+	
+	t2(Turtle turt) {
+		super(turt);
+		super.turtle = turt;
+
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
 }
