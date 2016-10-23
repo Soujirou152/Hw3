4802547eb7cfbd1035d2a2aa7e5cd9838fcd9441
pher100 <pher100@LH7U05CG62216KN>
2016-10-22 22:10:46 -0500

    Added turtle drawing capabilities

--- 602_hw_3/src/Pictures.java
+++ 602_hw_3/src/Pictures.java
@@ -1,9 +1,51 @@
+import java.util.ArrayList;
 
-public class Pictures
-{
+public class Pictures {
+	private ArrayList<TurtleDrawing> pictures;
 
-	public static void main(String[] args)
-	{
+	public Pictures() {
+		this.pictures = new ArrayList<TurtleDrawing>();
+	}
+
+	public void addPicture(TurtleDrawing td) {
+		this.pictures.add(td);
+	}
+
+	public void draw() {
+		for (TurtleDrawing td : pictures) {
+			td.draw();
+		}
+	}
+
+	public static void main(String[] args) {
+		double x0 = 0.25;
+		double y0 = 0.1;
+		double a0 = 0.0;
+
+		double x1 = 0.50;
+		double y1 = 0.25;
+		double a1 = 45.0;
+
+
+
+		Turtle turt1 = new Turtle(x0, y0, a0);
+		//Turtle turt2 = new Turtle(x1, y1, a1);
+		TurtleDrawing td1 = new t1(turt1);
+
+		TurtleDrawing td2 = new t2(turt1);
+
+		TurtleDrawing td3 = new CircleTurtleDrawing(turt1);
+
+		Pictures p = new Pictures();
+
+		p.addPicture(td1);
+		p.addPicture(td2);
+		p.addPicture(td3);
+
+		p.draw();
+		p.draw();
+		p.draw();
+		p.draw();
 
 	}
 
--- 602_hw_3/src/TurtleDrawing.java
+++ 602_hw_3/src/TurtleDrawing.java
@@ -1,50 +1,109 @@
 
-public abstract class TurtleDrawing
-{
+public abstract class TurtleDrawing {
 	protected Turtle turtle;
-	
-	TurtleDrawing(Turtle turt){
+
+	TurtleDrawing(Turtle turt) {
 		turtle = turt;
 	}
-	
+
 	public abstract void draw();
-	
-	
-	
-	public static void main(String[] args)
-	{
-		
+
+	public static void main(String[] args) {
+		double x0 = 0.5;
+		double y0 = 0.0;
+		double a0 = 60.0;
+
+		double x1 = 0.75;
+		double y1 = 0.25;
+		double a1 = 90.0;
+
+		Turtle t1 = new Turtle(x0, y0, a0);
+		TurtleDrawing td1 = new t1(t1);
+
+		Turtle t2 = new Turtle(x1, y1, a1);
+		TurtleDrawing td2 = new t2(t2);
+
+		td1.draw();
+		td2.draw();
 
 	}
 
 }
 
-class t1 extends TurtleDrawing{
-	
+class t1 extends TurtleDrawing {
+
 	t1(Turtle turt) {
 		super(turt);
-		super.turtle = turt;
-
 	}
 
 	@Override
 	public void draw() {
 		// TODO Auto-generated method stub
-		
+		double step = Math.sqrt(3) / 2;
+
+		super.turtle.penDown();
+		super.turtle.goForward(step);
+		super.turtle.turnLeft(120.0);
+		super.turtle.goForward(step);
+		super.turtle.turnLeft(120.0);
+		super.turtle.goForward(step);
+		super.turtle.turnLeft(120.0);
+		super.turtle.penUp();
+		super.turtle.goForward(.025);
 	}
 }
 
-class t2 extends TurtleDrawing{
-	
+class t2 extends TurtleDrawing {
+
 	t2(Turtle turt) {
 		super(turt);
-		super.turtle = turt;
-
 	}
 
 	@Override
 	public void draw() {
 		// TODO Auto-generated method stub
-		
+		double step = .5;
+
+		super.turtle.penDown();
+		super.turtle.goForward(step);
+		super.turtle.turnLeft(90.0);
+		super.turtle.goForward(step);
+		super.turtle.turnLeft(90.0);
+		super.turtle.goForward(step);
+		super.turtle.turnLeft(90.0);
+		super.turtle.goForward(step);
+		super.turtle.turnLeft(90.0);
+		super.turtle.penUp();
+		super.turtle.goForward(.025);
 	}
+}
+
+class CircleTurtleDrawing extends TurtleDrawing {
+
+	CircleTurtleDrawing(Turtle turt) {
+		super(turt);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+
+		for (int i = 0; i < 3; i++) {
+			super.turtle.penDown();
+			for (int j = 0; j < 360; j++) {
+				double radius = .05;
+				super.turtle.goForward(radius * .0174);
+				super.turtle.turnLeft(1);
+			}
+			super.turtle.penUp();
+//			super.turtle.turnLeft(150);
+			super.turtle.goForward(.18);
+		}
+		super.turtle.penUp();
+		super.turtle.turnLeft(90);
+		super.turtle.goForward(.025);
+
+	}
+
 }
